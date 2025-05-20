fe62f44b4c55723399543c13bfe36b797d4a5f5a
Bar Kupfershcmied <102284633+barkupf@users.noreply.github.com>
2025-05-20 17:40:31 +0300

    Add files via upload

---
+++ Lab2/PreTask/CCS/header/bsp_msp430x2xx.h
@@ -0,0 +1,119 @@
+#ifndef _bsp_H_
+#define _bsp_H_
+
+#include  <msp430g2553.h>          // MSP430x2xx
+
+
+#define   debounceVal      250
+#define   LEDs_SHOW_RATE   0xFFFF  // 62_5ms
+
+// Switches abstraction
+#define SWsArrPort         P2IN
+#define SWsArrPortDir      P2DIR
+#define SWsArrPortSel      P2SEL
+#define SWmask             0xF0
+#define SWsArrIntPend      P2IFG
+#define SW0                0x01
+
+// PushButtons abstraction
+#define PBsArrPort	       P1IN
+#define PBsArrIntPend	   P1IFG 
+#define PBsArrIntEn	       P1IE
+#define PBsArrIntEdgeSel   P1IES
+#define PBsArrPortSel      P1SEL 
+#define PBsArrPortDir      P1DIR 
+#define PB0                0x01
+#define PB1                0x02
+#define PB2                0x04
+#define PB3                0x08
+
+// Buzzer  abstraction
+#define BuzzerArrPort	   P2OUT
+#define BuzzerArrPortDir   P2DIR
+#define BuzzerArrPortSel   P2SEL
+
+// Timer abstraction
+#define TimerA1_Dir			P2DIR
+#define TimerA1_Sel			P2SEL
+#define SMCLK_freq          1114000 // 2^20 (measured 1.111~1.114Mhz)
+
+// LCDs abstraction
+
+// #define CHECKBUSY	1  // using this define, only if we want to read from LCD
+#ifdef CHECKBUSY
+    #define LCD_WAIT lcd_check_busy()
+#else
+    #define LCD_WAIT DelayMs(5)
+#endif
+
+/*----------------------------------------------------------
+  CONFIG: change values according to your port pin selection
+------------------------------------------------------------*/
+#define LCD_CTRL_WRITE  P2OUT // LCD control lines connected to P2.5-P2.7
+#define LCD_CTRL_DIR    P2DIR
+#define LCD_CTRL_READ   P2IN
+#define LCD_CTRL_SEL    P2SEL
+
+#define LCD_EN(a)       (a ? (LCD_CTRL_WRITE |= 0x20) : (LCD_CTRL_WRITE &= ~0x20)) // P2.5 is lcd enable pin
+#define LCD_EN_DIR(a)   (a ? (LCD_CTRL_DIR |= 0x20) : (LCD_CTRL_DIR &= ~0x20)) // P2.5 pin direction
+
+#define LCD_RS(a)       (a ? (LCD_CTRL_WRITE |=0x40) : (LCD_CTRL_WRITE &=~0x40)) // P2.6 is lcd RS pin
+#define LCD_RS_DIR(a)   (a ? (LCD_CTRL_DIR |= 0x40) : (LCD_CTRL_DIR &= ~0x40)) //P2.6, direction pin
+
+#define LCD_RW(a)       (a ? (LCD_CTRL_WRITE |=0x80) : (LCD_CTRL_WRITE &=~0x80)) // P2.7 is lcd RW pin
+#define LCD_RW_DIR(a)   (a ? (LCD_CTRL_DIR |=0x80) : (LCD_CTRL_DIR &=~0x80)) // P2.7 pin direction
+
+
+#define LCD_DATA_OFFSET 0x04 	// Data pin selection offset for 4 bit mode, variable range is 0-4, default 0 - Px.0-3, no offset
+#define LCD_DATA_WRITE  P1OUT   // Output port for the LCD screen
+#define LCD_DATA_DIR    P1DIR
+#define LCD_DATA_READ   P1IN
+/*---------------------------------------------------------
+  END CONFIG
+-----------------------------------------------------------*/
+#define FOURBIT_MODE    0x0
+#define EIGHTBIT_MODE   0x1
+#define LCD_MODE        FOURBIT_MODE
+
+#define OUTPUT_PIN      1
+#define INPUT_PIN       0
+#define OUTPUT_DATA     (LCD_MODE ? 0xFF : (0x0F << LCD_DATA_OFFSET))
+#define INPUT_DATA      0x00
+
+#define LCD_STROBE_READ(value)  LCD_EN(1), \
+                asm("nop"), asm("nop"), \
+                value=LCD_DATA_READ, \
+                LCD_EN(0)					// This is the trigger rutine in the tutorial
+
+// Might come in handy, need to verify they are all suitable for 4-bit mode
+#define lcd_cursor(x)       lcd_cmd(((x)&0x7F)|0x80) // ????
+#define lcd_clear           lcd_cmd(0x01)
+#define lcd_putchar(x)      lcd_data(x)
+#define lcd_goto(x)         lcd_cmd(0x80+(x)) // ????
+#define lcd_cursor_right()  lcd_cmd(0x14)
+#define lcd_cursor_left()   lcd_cmd(0x10)
+#define lcd_display_shift() lcd_cmd(0x1C) // ????
+#define lcd_home            lcd_cmd(0x02)
+#define cursor_off          lcd_cmd(0x0C)
+#define cursor_on           lcd_cmd(0x0F)
+#define lcd_function_set    lcd_cmd(0x3C) // 8bit,two lines,5x10 dots
+#define lcd_new_line        lcd_cmd(0xC0)
+#define lcd_entry_mode      lcd_cmd(0x6)
+
+
+extern void GPIOconfig(void);
+extern void TIMERconfig(void);
+extern void ADCconfig(void);
+extern void lcd_cmd(unsigned char c);
+extern void lcd_data(unsigned char c);
+extern void lcd_puts(unsigned char* s);
+extern void lcd_init();
+extern void DelayUs(unsigned int cnt);
+extern void DelayMs(unsigned int cnt);
+extern void lcd_strobe();
+
+
+#endif
+
+
+
---
+++ Lab2/PreTask/CCS/source/bsp.c
@@ -0,0 +1,210 @@
+#include  "../header/bsp_msp430x2xx.h"    // private library - BSP layer, for personal kit
+//#include  "../header/bsp_msp430x4xx.h"    // private library - BSP layer, for lab kit
+
+//-----------------------------------------------------------------------------  
+//           GPIO congiguration
+//-----------------------------------------------------------------------------
+void GPIOconfig(void){
+ // volatile unsigned int i; // in case of while loop usage
+  
+  WDTCTL = WDTHOLD | WDTPW;			 // Stop WDT
+  
+  // Switches Setup					 // SW0
+  SWsArrPortSel &= ~0x01;            // I/O
+  SWsArrPortDir &= ~0x01;            // Input
+
+  // PushButtons Setup				 // PB0,PB1,PB2
+  PBsArrPortSel &= ~0x07;            // I/O
+  PBsArrPortDir &= ~0x07;            // Input
+  PBsArrIntEdgeSel |= 0x03;  	     // pull-up mode
+  PBsArrIntEdgeSel &= ~0x04;         // pull-down mode
+  PBsArrIntEn |= 0x07;
+  PBsArrIntPend &= ~0xFF;            // clear pending interrupts
+  
+  // Buzzer Setup
+  BuzzerArrPortDir |= 0x04;			// P2.2 Output compare - '1'
+  BuzzerArrPortSel |= 0x04;         // P2.2 Select = '1'
+  BuzzerArrPort    &= ~0x04;        // P2.2 out = '0'
+
+  _BIS_SR(GIE);                      // enable interrupts globally
+}                             
+//------------------------------------------------------------------------------------- 
+//            Timers congiguration 
+//-------------------------------------------------------------------------------------
+void TIMERconfig(void){
+	
+	// Initial configuration of Timers - put them on hold, enable&disable (incl. IE) upon state entrance
+	TimerA1_Dir &= ~0x10; // P2.4 set to Input
+	TimerA1_Sel |= 0x10; // P2.4 'select' set to 1 to set as Timer A
+	
+	// State 1
+	TA1CTL = TASSEL_2 + TACLR; // SMCLK input, cleared current value
+	TA1CCTL2 = CM_1 + SCS + CAP; // Capture on rising edge, synchronous capture, capture mode
+	
+	// State 2
+	TA0CTL = TASSEL_2 + ID_3 + TACLR; // SMCLK input divided by 8, cleared current value
+	TA0CCR0 = 0x8FFF;
+
+	// State 3
+	TA1CCTL1 = OUTMOD_0;
+}
+//------------------------------------------------------------------------------------- 
+//            ADC congiguration 
+//-------------------------------------------------------------------------------------
+void ADCconfig(void){
+	
+	ADC10CTL0 &= ~ENC;
+	ADC10CTL0 = SREF_0 + ADC10SHT_2 + ADC10ON + ADC10IE; // V_R+=Vcc & V_R-=Vss, Sample&hold time = 16 x ADC10CLKs, Turn on, Enable interrupts
+	ADC10CTL1 = INCH_3 + ADC10SSEL_3;	// Input channel A3(P1.3), ADC10CLK = SMCLK
+	ADC10AE0 |= BIT3;
+}              
+
+//------------------------------------------------------------------------------------- 
+//            LCD configuration
+//-------------------------------------------------------------------------------------
+
+//******************************************************************
+// send a command to the LCD
+//******************************************************************
+void lcd_cmd(unsigned char c){
+  /* Sends a command to the LCD (using LCD_DATA_WRITE as output port)
+  // Using 4 bit-mode, it first sets the 4 MSBs then the 4 LSBs
+  // The LCD_DATA_WRITE is used as 4-bits-long (rest are kept as-is) */
+  
+	LCD_WAIT; // may check LCD busy flag, or just delay a little, depending on lcd.h
+
+	if (LCD_MODE == FOURBIT_MODE)
+	{
+		LCD_DATA_WRITE &= ~OUTPUT_DATA;// clear bits before new write
+                LCD_DATA_WRITE |= ((c >> 4) & 0x0F) << LCD_DATA_OFFSET;
+		lcd_strobe();
+                LCD_DATA_WRITE &= ~OUTPUT_DATA;
+    		LCD_DATA_WRITE |= (c & (0x0F)) << LCD_DATA_OFFSET;
+		lcd_strobe();
+	}
+	else
+	{
+		LCD_DATA_WRITE = c;
+		lcd_strobe();
+	}
+}
+//******************************************************************
+// send data to the LCD
+//******************************************************************
+void lcd_data(unsigned char c){
+        
+	LCD_WAIT; // may check LCD busy flag, or just delay a little, depending on lcd.h
+
+	LCD_DATA_WRITE &= ~OUTPUT_DATA;       
+	LCD_RS(1);
+	if (LCD_MODE == FOURBIT_MODE)
+	{
+    		LCD_DATA_WRITE &= ~OUTPUT_DATA;
+                LCD_DATA_WRITE |= ((c >> 4) & 0x0F) << LCD_DATA_OFFSET;  
+		lcd_strobe();		
+                LCD_DATA_WRITE &= (0xF0 << LCD_DATA_OFFSET) | (0xF0 >> 8 - LCD_DATA_OFFSET);
+                LCD_DATA_WRITE &= ~OUTPUT_DATA;
+		LCD_DATA_WRITE |= (c & 0x0F) << LCD_DATA_OFFSET; 
+		lcd_strobe();
+	}
+	else
+	{
+		LCD_DATA_WRITE = c;
+		lcd_strobe();
+	}
+          
+	LCD_RS(0);   
+}
+//******************************************************************
+// write a string of chars to the LCD
+//******************************************************************
+void lcd_puts(unsigned char* s){
+  
+	while(*s) //when the array of characters reaches the NULL character (00), breaks
+		lcd_data(*s++);
+}
+//******************************************************************
+// initialize the LCD
+//******************************************************************
+void lcd_init(){
+  
+	char init_value;
+
+	if (LCD_MODE == FOURBIT_MODE) init_value = 0x3 << LCD_DATA_OFFSET;
+        else init_value = 0x3F;
+	LCD_CTRL_SEL &= ~0xE0;
+	LCD_RS_DIR(OUTPUT_PIN); // output mode for all RS+EN+RW
+	LCD_EN_DIR(OUTPUT_PIN);
+	LCD_RW_DIR(OUTPUT_PIN);
+	LCD_DATA_DIR |= OUTPUT_DATA; // output mode for 4 "data" bits
+	LCD_RS(0); // "data contains instruction code"
+	LCD_EN(0); // enable signal = 0 (LCD transition when 1->0)
+	LCD_RW(0); // write mode
+        
+	DelayMs(15);
+    
+	LCD_DATA_WRITE &= ~OUTPUT_DATA; // data channel to 0x0
+	LCD_DATA_WRITE |= init_value;	// data channel to 4'b0011
+	lcd_strobe();					// update screen & advance to next 'matrix'
+	DelayMs(5);						
+	LCD_DATA_WRITE &= ~OUTPUT_DATA;	// data channel to 4'b0000
+	LCD_DATA_WRITE |= init_value;	// data channel to 4'b0011
+	lcd_strobe();					// ....
+	DelayUs(200);					// 200 Microsecond delay
+	LCD_DATA_WRITE &= ~OUTPUT_DATA; // data channel to 4'b0000
+	LCD_DATA_WRITE |= init_value;	// data channel to 4'b0011
+	lcd_strobe();					// ....
+	
+	if (LCD_MODE == FOURBIT_MODE)
+	{
+		LCD_WAIT; // may check LCD busy flag, or just delay a little, depending on lcd.h
+		LCD_DATA_WRITE &= ~OUTPUT_DATA;
+		LCD_DATA_WRITE |= 0x2 << LCD_DATA_OFFSET; // Set 4-bit mode
+		lcd_strobe();
+		lcd_cmd(0x28); // Function Set - configuartion of screen
+	}
+	else lcd_function_set; // 8bit,two lines,5x10 dots 
+	
+	cursor_on;
+	lcd_clear;
+	lcd_entry_mode; //Entry Mode
+	lcd_goto(0);//Initialize DDRAM address to zero
+	lcd_home; // move cursor to start (defined in header)
+}
+//******************************************************************
+// Delay usec functions
+//******************************************************************
+void DelayUs(unsigned int cnt){
+  
+	unsigned char i;
+        for(i=cnt ; i>0 ; i--)
+            __no_operation();
+}
+//******************************************************************
+// Delay msec functions
+//******************************************************************
+void DelayMs(unsigned int cnt){
+  
+	unsigned char i;
+        for(i=cnt ; i>0 ; i--) DelayUs(1000); // tha command asm("nop") takes raphly 1usec
+	
+}
+//******************************************************************
+// lcd strobe functions
+//******************************************************************
+void lcd_strobe(){
+  LCD_EN(1);
+  __no_operation();
+  __no_operation();
+  LCD_EN(0);
+}
+
+           
+             
+
+ 
+             
+             
+            
+  
+
